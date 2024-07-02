6df9614d8946c6a6d26e84b7a8818327fa2c2b18
raymondjgallant24 <raymond.gallant@enmu.edu>
2024-07-02 14:37:28 -0600

    use lists to print students

--- hogwarts.py
+++ hogwarts.py
@@ -1,4 +1,5 @@
 students = ["Hermione", "Harry", "Ron"]
-#print(f"{students[0], students[1], students[2]}")
-for student in students:
-    print(student)
+for i in range(len(students)):
+    print(i + 1, students[i])
+    
+
