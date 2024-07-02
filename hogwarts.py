744261a87ec6a514f98dbcd55554c3bc532db66f
raymondjgallant24 <raymond.gallant@enmu.edu>
2024-07-02 15:15:01 -0600

    use a list of dicts to create table

--- hogwarts.py
+++ hogwarts.py
@@ -1,10 +1,8 @@
-students = {
-            "Hermoine":"Gryffindor", 
-            "Harry":"Gryffindor",
-            "Ron":"Gryffindor",
-            "Draco":"Slytherin"
-            }
-
+students = [
+        {"name": "Hermione", "house":"Gryffindor","patronus":"Otter"},
+        {"name": "Harry", "house":"Gryffindor","patronus":"Stag"},
+        {"name": "Ron", "house":"Gryffindor","patronus":"Jack Russel Terrier"},
+        {"name": "Draco", "house":"Slytherin","patronus":None}
+        ]
 for student in students:
-    print(student, students[student], sep = ", ")
-
+    print(student["name"],student["house"],student["patronus"], sep=", ")
