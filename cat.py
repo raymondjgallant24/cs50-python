acdf4ca348caac67d47921507e50513fe88606ec
raymondjgallant24 <raymond.gallant@enmu.edu>
2024-07-02 14:21:21 -0600

    create funcs to enable meow func

--- cat.py
+++ cat.py
@@ -1,2 +1,19 @@
-for i in [0, 1, 2,]:
-    print("Meow")
+def main():
+    number = get_number()
+    meow(number)
+
+
+def get_number():
+        while True:
+            n = int(input("What's n? "))
+            if n >0:
+                break
+        return n
+    
+
+
+def meow(n):
+        for _ in range(n):
+            print("meow")
+
+main()
