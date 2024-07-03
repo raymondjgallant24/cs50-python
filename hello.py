a02bbfd42a9ddd036dda4ff982ab0e046dccd1a4
raymondjgallant24 <raymond.gallant@enmu.edu>
2024-07-03 09:33:25 -0600

    implement try except

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 def main():
-    name = input("What's your name? ")
-    hello()
+    name = input("What's your name? ").strip().capitalize()
+    hello(name)
+    return name
 
-def hello():
-    print("hello,", to)
+
+def hello(name):
+    print(f"hello, {name}")
 
 main()
