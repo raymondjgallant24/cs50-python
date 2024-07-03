17dc7cd9c591d0fb52c599188a692f7036f70bf6
raymondjgallant24 <raymond.gallant@enmu.edu>
2024-07-03 09:42:55 -0600

    return x in try state.

--- number.py
+++ number.py
@@ -1,9 +1,16 @@
-while True:
-    try:
-        x = int(input("What's x? "))
-        
-    except ValueError:
-        print("x is not an integer")
-    else:    
-        print(f"x is {x}")
-        break
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+
+
+def get_int():    
+    while True:
+            try:
+                x = int(input("What's x? "))
+                return x
+            except ValueError:
+                print("x is not an integer")
+  
+                
+main()
