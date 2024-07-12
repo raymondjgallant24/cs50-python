2272ef67e7a23e5cc1a6622bc96b21d4eb6f531c
raymondjgallant24 <raymond.gallant@enmu.edu>
2024-07-12 11:40:21 -0600

    change from "w" to "a"

--- names.py
+++ names.py
@@ -1,6 +1,6 @@
 name = input("What's your name? ")
 
-file = open("names.txt", "w") #"w" recreates the file everytime 
+file = open("names.txt", "a") #"w" recreates the file everytime "a" will append to the bottom of the file
 
 file.write(name)
 file.close()
